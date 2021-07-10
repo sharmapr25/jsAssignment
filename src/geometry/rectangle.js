b5dd5f76ad4189017e960695598e8f246872e26c
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-07-10 11:28:19 +0530

    Removed square class and created a factory method for creating square in rectangle

--- src/geometry/rectangle.js
+++ src/geometry/rectangle.js
@@ -1,17 +1,20 @@
-class Rectangle{
-  constructor(width, length){
+class Rectangle {
+  constructor(width, length) {
     this.width = width;
     this.length = length;
-  };
+  }
 
-  area(){
+  static createSquare(size){
+    return new Rectangle(size, size);
+  }
+
+  area() {
     return this.width * this.length;
-  };
+  }
 
-  perimeter(){
-    return 2 * ( this.width + this.length );
-  };
-
+  perimeter() {
+    return 2 * (this.width + this.length);
+  }
 };
 
 module.exports = Rectangle;
--- src/geometry/square.test.js
+++
@@ -1,6 +0,0 @@
-const Square = require('./square');
-
-test("area should return 4 when given square size is 2", () => {
-  const square = new Square(2);
-  expect(square.area()).toBe(4);
-});
