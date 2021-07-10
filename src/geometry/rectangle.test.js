80ef2217d448fdc28bcbab604c27b10ff48b2600
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-07-10 11:29:49 +0530

    Written tests for square perimeter

--- src/geometry/rectangle.test.js
+++ src/geometry/rectangle.test.js
@@ -15,3 +15,8 @@ test("perimeter should return 10 when given rectangle width is 3 and length is 2
   expect(rectangle.perimeter()).toBe(10);
 });
 
+test("perimeter should return 8 when given square size is 2", () => {
+  const square = Rectangle.createSquare(2);
+  expect(square.perimeter()).toBe(8);
+});
+
