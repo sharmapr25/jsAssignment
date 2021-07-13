8bebc9a59a8e36dcc8ab2671a1e329143dea9b41
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-07-13 15:44:59 +0530

    Written test for checking 1000m equals to 1 km

--- src/quantity/measurement.test.js
+++ src/quantity/measurement.test.js
@@ -15,3 +15,18 @@ test("equals return false when 200 centimeter is not equal to 1 meter", () => {
   expect(hundredCentimeter.equals(oneMeter)).toBeFalsy();
 });
 
+test("equals return true when 1000 meter is equal to 1 kilometer", () => {
+  const thousandMeter = new Measurement(1000, Unit.M);
+  const oneKiloMeter = new Measurement(1, Unit.KM);
+
+  expect(thousandMeter.equals(oneKiloMeter)).toBeTruthy();
+});
+
+test("equals return false when 2000 meter is not equal to 1 kilometer", () => {
+  const twoThousandMeter = new Measurement(2000, Unit.M);
+  const oneKiloMeter = new Measurement(1, Unit.KM);
+
+  expect(twoThousandMeter.equals(oneKiloMeter)).toBeFalsy();
+});
+
+
--- src/quantity/unit.js
+++ src/quantity/unit.js
@@ -1,6 +1,7 @@
 const Unit = {
   CM: 1,
-  M: 0.01
+  M: 0.01,
+  KM: 0.00001
 };
 
 Object.freeze(Unit);
