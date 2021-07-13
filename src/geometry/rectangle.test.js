4e0fc3f37d228267dac0562d6a564aeda13221fc
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-07-13 12:55:30 +0530

    Removed should word from all tests for consistency

--- src/geometry/rectangle.test.js
+++ src/geometry/rectangle.test.js
@@ -1,21 +1,21 @@
 const Rectangle = require('./rectangle');
 
-test('area should return 6 when given rectangle width is 3 and length is 2', () => {
+test('area return 6 when given rectangle width is 3 and length is 2', () => {
   const rectangle = new Rectangle(3, 2);
   expect(rectangle.area()).toBe(6);
 });
 
-test("area should return 4 when given square size is 2", () => {
+test("area return 4 when given square size is 2", () => {
   const square = Rectangle.createSquare(2);
   expect(square.area()).toBe(4);
 });
 
-test("perimeter should return 10 when given rectangle width is 3 and length is 2", () => {
+test("perimeter return 10 when given rectangle width is 3 and length is 2", () => {
   const rectangle = new Rectangle(3, 2);
   expect(rectangle.perimeter()).toBe(10);
 });
 
-test("perimeter should return 8 when given square size is 2", () => {
+test("perimeter return 8 when given square size is 2", () => {
   const square = Rectangle.createSquare(2);
   expect(square.perimeter()).toBe(8);
 });
--- src/probability/probability.test.js
+++ src/probability/probability.test.js
@@ -1,6 +1,9 @@
 const Probability = require('./probability');
+
 test('equals return true when two probabilities are equal', () => {
   const probability1 = new Probability(0.5);
   const probability2 = new Probability(0.5);
   expect(probability1.equals(probability2)).toBeTruthy();
 });
+
+
