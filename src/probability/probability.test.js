fa472bdff04f84e2c605d3a8e91088a4f0ef7929
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-07-13 12:56:42 +0530

    Written another test for returning false when two probabilities are not same

--- src/probability/probability.test.js
+++ src/probability/probability.test.js
@@ -6,4 +6,9 @@ test('equals return true when two probabilities are equal', () => {
   expect(probability1.equals(probability2)).toBeTruthy();
 });
 
+test("equals return false when two probabilities are not equal", () => {
+  const probability1 = new Probability(0.5);
+  const probability2 = new Probability(0.7);
+  expect(probability1.equals(probability2)).toBeFalsy();
+});
 
