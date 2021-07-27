7977549c1175f24f10df60c2fc7046ddb9bcbe18
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-07-27 09:27:07 +0530

    Requirement-9 | Added test for managing multiple parking lots by attendant

--- src/parkingLot/attendant.js
+++ src/parkingLot/attendant.js
@@ -1,16 +1,25 @@
 const Observer = require('./observer');
 
-class Attendant extends Observer{
-  constructor(parkingLot){
-    this.parkingLot = parkingLot;
+class Attendant extends Observer {
+  constructor(parkingLots) {
+    this.parkingLots = parkingLots;
+    this.availableLots = parkingLots;
   }
 
-  park(car){
-    this.parkingLot.park(car);
+  park(car) {
+    this.availableLots[0].park(car);
   }
 
-  unpark(car){
-    this.parkingLot.unpark(car);
+  unpark(car) {
+    this.parkingLots.find((parkingLot) => parkingLot.isParked(car)).unpark(car);
+  }
+
+  notifyWhenSpaceNotAvailable(parkingLotIndex) {
+    this.availableLots.splice(parkingLotIndex, 1);
+  }
+
+  notifyWhenSpaceIsAvailable(parkingLot) {
+    this.availableLots.push(parkingLot)
   }
 }
 
--- src/parkingLot/attendant.test.js
+++ src/parkingLot/attendant.test.js
@@ -4,7 +4,7 @@ const Attendant = require('./attendant');
 describe('attendant', () => {
   it('park parked the car when space is available', () => {
     const parkingLot = new ParkingLot(1);
-    const attendant = new Attendant(parkingLot);
+    const attendant = new Attendant([parkingLot]);
     const car = { number: 112 };
 
     attendant.park(car);
@@ -12,11 +12,21 @@ describe('attendant', () => {
 
   it("unpark unparked the car when car is available in parking lot", () => {
     const parkingLot = new ParkingLot(1);
-    const attendant = new Attendant(parkingLot);
+    const attendant = new Attendant([parkingLot]);
     const car = { number: 112 };
 
     attendant.park(car);
     attendant.unpark(car);
 
+  });
+
+  it("park parked the car in second parking lot when first parking lot is full", () => {
+    const parkingLot = new ParkingLot(1);
+    const anotherParkingLot = new ParkingLot(2);
+    const attendant = new Attendant([parkingLot, anotherParkingLot]);
+    const car = { number: 112 };
+
+    attendant.park(car);
+    
   });
 });
